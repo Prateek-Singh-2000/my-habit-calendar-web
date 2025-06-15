b05691621f9f471ad82c50bc78f4078096ed1dee
Prateek-Singh-2000 <prateekmt787@gmail.com>
2025-06-15 14:14:40 +0530

    Added new profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import DailyHabitsPage from "./pages/DailyHabitsPage/DailyHabitsPage";
 import "./App.css";
 
 function App() {
-	return (
-		<div className="app-container">
-			<Routes>
-				<Route path="/" element={<DailyHabitsPage />} />
-				<Route path="*" element={<h2>404 - Page Not Found</h2>} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="app-container">
+      <Routes>
+        <Route path="/" element={<DailyHabitsPage />} />
+        <Route path="*" element={<h2>404 - Page Not Found</h2>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,16 @@
+function ProfilePage() {
+  return (
+    <div className="profile-page">
+      <div className="profile-photo-holder">
+        <img
+          src="https://via.placeholder.com/150"
+          alt="Profile"
+          className="profile-photo"
+        />
+      </div>
+      <h2 className="profile-name">John Doe</h2>
+    </div>
+  );
+}
+
+export default ProfilePage;
