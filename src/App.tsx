423ac88750691fc231a16cab32b0fd60aaa315de
Prateek-Singh-2000 <prateekmt787@gmail.com>
2025-06-15 14:59:10 +0530

    Added a second page for profile based information

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import DailyHabitsPage from "./pages/DailyHabitsPage/DailyHabitsPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="app-container">
-      <Routes>
-        <Route path="/" element={<DailyHabitsPage />} />
-        <Route path="*" element={<h2>404 - Page Not Found</h2>} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="app-container">
+			<div className="global-nav">
+				<Link to="/" className="nav-link">
+					Daily Habits
+				</Link>
+				<Link to="/profile" className="nav-link">
+					Profile
+				</Link>
+			</div>
+			<Routes>
+				<Route path="/" element={<DailyHabitsPage />} />
+				<Route path="/profile" element={<ProfilePage />} />
+				<Route path="*" element={<h2>404 - Page Not Found</h2>} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,16 +1,20 @@
+import "./ProfilePage.css";
+
 function ProfilePage() {
-  return (
-    <div className="profile-page">
-      <div className="profile-photo-holder">
-        <img
-          src="https://via.placeholder.com/150"
-          alt="Profile"
-          className="profile-photo"
-        />
-      </div>
-      <h2 className="profile-name">John Doe</h2>
-    </div>
-  );
+	// Hard code values for now
+	const name: string = "John Doe";
+	const email: string = "john.doe@gmail.com";
+	const userPhotoUrl = "https://avatar.iran.liara.run/public/48";
+	return (
+		<div className="profile-page">
+			<h1>Profile Page</h1>
+			<div className="profile-information">
+				<img src={userPhotoUrl} alt="Profile" className="profile-photo" />
+				<h3 className="profile-name profile-element">{name}</h3>
+				<p className="email-name profile-element">{email}</p>
+			</div>
+		</div>
+	);
 }
 
 export default ProfilePage;
